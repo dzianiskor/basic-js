bd2149cadd68e9b5eba8c31e59263c3dfef4b20e
Denis Korzun <dzianiskor@gmail.com>
2021-03-29 20:33:37 +0300

    transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('Not Array')
+    }
+    const clonedArray = [...arr]
+    const newArray = []
+    for (let i = 0; i < clonedArray.length; i++) {
+        switch (clonedArray[i]) {
+            case '--discard-next':
+                if (i < clonedArray.length - 1) {
+                    clonedArray[i+1] = null
+                }
+                i++;
+                break;
+            case '--discard-prev':
+                if (i > 0 && clonedArray[i-1] !== null) {
+                    clonedArray[i-1] = null
+                    newArray.pop()
+                }
+                break;
+            case '--double-next':
+                if (i < clonedArray.length - 1 && clonedArray[i + 1]!==null) {
+                    newArray.push(clonedArray[i + 1])
+                }
+                break;
+            case '--double-prev':
+                if (i > 0 && clonedArray[i - 1]!==null) {
+                    newArray.push(clonedArray[i - 1])
+                }
+                break;
+            default:
+                newArray.push(clonedArray[i])
+        }
+    }
+
+    return newArray
+}
